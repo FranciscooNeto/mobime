350bb4369d7c7ef46550875bba8aa1e4b6601348
Francisco Neto <98996265+FranciscooNeto@users.noreply.github.com>
2022-04-13 10:18:34 -0300

    Definindo Rotas!

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import history from "./services/history.js";
 import RoutesM from "./routes/index.js";
-
+import LoginPage from "./pages/loginPage.js";
 
 function App() {
   return (
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,15 +4,20 @@ import { Route, Routes } from "react-router-dom";
 
 import LoginPage from '../pages/loginPage'
 import loginRoute from "../routes/loginRoute";
+import ListarPage from "../pages/ListarPage";
+import SobrePage from "../pages/SobrePage";
+
 
 function RoutesM(){
   return(
-  
+
     <Routes>
-    <Route path="/" element={<loginRoute />}>
-      <Route exact path="/login" element={<LoginPage />}/>
-    </Route>
-    </Routes>
+    <Route path="/" element={<loginRoute />}/>
+    <Route exact path="/login" element={<LoginPage />}/>
+    <Route exact path="/listar" element={<ListarPage />}/>
+    <Route exact path="/sobre" element={<SobrePage />}/>
+    
+   </Routes>
 
   )}
 
