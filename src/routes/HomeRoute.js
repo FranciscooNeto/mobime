2c4c6a3733cb1bc9e78fabf0325554ff3ddfa850
Francisco Neto <98996265+FranciscooNeto@users.noreply.github.com>
2022-05-02 10:22:07 -0300

    Rotas

--- src/routes/HomeRoute.js
+++ src/routes/HomeRoute.js
@@ -1,7 +1,6 @@
 import { Navigate } from "react-router-dom";
-
 const homeRoute = ()=>{
-    return <Navigate to={{ pathname: "/" }} />;
+    return <Navigate to={{ pathname: "/login" }} />;
 }
 
 export default homeRoute;
